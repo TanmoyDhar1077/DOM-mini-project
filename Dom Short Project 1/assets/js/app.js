e87e52184c6faada5b7c21c6d15f3ec2556bf74c
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-02-11 00:50:53 +0600

    Project 1 Done

---
+++ Dom Short Project 1/assets/js/app.js
@@ -0,0 +1,36 @@
+const numCount = document.getElementById('counter');
+const plusBtn = document.getElementById('increment');
+const mainusBtn = document.getElementById('decrement');
+
+let count = 0;
+
+const updateCount = (value) => {
+    count = count + value;
+    numCount.textContent = count;
+
+
+    // for plus button
+    if (count >= 10) {
+        plusBtn.setAttribute('disabled', true);
+    }else {
+        plusBtn.removeAttribute('disabled', false);
+    }
+
+
+    // for minus button
+    if (count <= 0) {
+        mainusBtn.setAttribute('disabled', true);
+    }else {
+        mainusBtn.removeAttribute('disabled', false);
+    }
+
+};
+
+
+plusBtn.addEventListener('click', () => {
+     updateCount(1);  
+});
+
+mainusBtn.addEventListener('click', () => {
+    updateCount(-1);
+});
